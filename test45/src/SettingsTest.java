2534c1f60d5019a83ec2c71ec626132bc68da523
Mansi Virmani <mansi.v@directi.com>
2014-10-09 14:15:25 +0530

    Scrollable added to init assignment

--- test45/src/SettingsTest.java
+++ test45/src/SettingsTest.java
@@ -1,7 +1,5 @@
-import com.android.uiautomator.core.UiCollection;
-import com.android.uiautomator.core.UiObject;
-import com.android.uiautomator.core.UiObjectNotFoundException;
-import com.android.uiautomator.core.UiSelector;
+import android.widget.LinearLayout;
+import com.android.uiautomator.core.*;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 /**
@@ -17,15 +15,40 @@ public class SettingsTest extends UiAutomatorTestCase {
         UiObject setingsIcon_UiObject = new UiObject(settingIcon_UiSelector);
 
         if (setingsIcon_UiObject.click()) {
-            UiCollection Linear=new UiCollection(new UiSelector().packageName("com.android.settings").className("android.widget.LinearLayout").resourceId("com.android.settings:id/header_connection_airplane_mode"));
-            UiObject AirPlane_CheckBox=Linear.getChild(new UiSelector().resourceId("com.android.settings:id/check_check"));
-            if (!AirPlane_CheckBox.isChecked()) {
-                AirPlane_CheckBox.click();
-                System.out.println("Success");
 
-            } else {
-                System.out.println("Failure");
+//Snippet to turn device into airplane mode if it is not.
+//            UiCollection Linear=new UiCollection(new UiSelector().packageName("com.android.settings").className("android.widget.LinearLayout").resourceId("com.android.settings:id/header_connection_airplane_mode"));
+//            UiObject AirPlane_CheckBox=Linear.getChild(new UiSelector().resourceId("com.android.settings:id/check_check"));
+//            if (!AirPlane_CheckBox.isChecked()) {
+//                AirPlane_CheckBox.click();
+//                System.out.println("Success");
+//
+//            } else {
+//                System.out.println("Failure");
+//            }
+
+            // Snippet to try UIScrollable
+            UiScrollable ScrollDownSettings = new UiScrollable(new UiSelector().className("android.widget.ListView"));
+            if (ScrollDownSettings.isScrollable()) {
+                ScrollDownSettings.setAsVerticalList();
+                UiObject NearByDevices = ScrollDownSettings.getChildByText(new UiSelector().className("android.widget.TextView"),"Nearby devices");
+                Boolean Caught=false;
+                do {
+                    Caught=ScrollDownSettings.scrollIntoView(NearByDevices);
+                    System.out.println("Scroll Attempt:" + Caught.toString());
+                }while( Caught!= true);
+
+                if ( Caught==true && NearByDevices.click())
+                    System.out.println("Success");
+                else
+                    System.out.println("Failure:Could not locate object on scroll");
+            }
+            else
+            {
+                System.out.println("Failure: could not scroll");
             }
         }
+        else
+            System.out.println("Failure:Could not open Settings");
     }
 }
