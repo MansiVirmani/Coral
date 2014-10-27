1b07d9d0924c86dad1d62c162e401eb34c6730f8
Mansi Virmani <mansi.v@directi.com>
2014-10-27 14:52:43 +0530

    remove bin folder from tracking

--- Tringoo/bin/classes.dex.d
+++
@@ -1,4 +0,0 @@
-/home/mansi/IdeaProjects/Tringoo/bin/classes.dex : \
-/home/mansi/IdeaProjects/Tringoo/bin/classes/TringooLogger.class \
-/home/mansi/IdeaProjects/Tringoo/bin/classes/TringooUtils.class \
-/home/mansi/IdeaProjects/Tringoo/bin/classes/SignUp.class \
